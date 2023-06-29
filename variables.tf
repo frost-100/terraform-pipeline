98ee4915c689dcb8da458f56c2833c5042f26593
frost-100 <olamideademowo@yahoo.com>
2023-06-29 14:35:25 -0400

    change

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "my-access-key" {
+    description = "this is key"
+    type =  string
+    default = "AKIA2OCKVF2ZAR2SHS62"
+  
+}
+variable "my-secret-key" {
+    description = "this is secret key"
+    type = string
+    default = "4gxBeyfhoKSUoQ2lY09lYMxRhmxMKvpJeU38L78a"
+  
+}
+variable "my-region" {
+    description = "this my region"
+    type = string
+    default = "us-east-1"
+  
+}
